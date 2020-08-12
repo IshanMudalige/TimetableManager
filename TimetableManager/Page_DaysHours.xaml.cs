c8e8a2b1a35f8ce14425e4b6145587df5c29309e
IshanMudalige <adithaishan@gmail.com>
2020-08-12 23:21:52 +0530

    Working days & Hours insert/display/search

--- TimetableManager/Page_DaysHours.xaml.cs
+++ TimetableManager/Page_DaysHours.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,19 +27,148 @@ namespace TimetableManager
         {
             InitializeComponent();
 
-            Weekday week = new Weekday();
-            week.Title = "week 6";
-            week.Hours = 54.45;
-            week.No_days = 4;
-            week.Slots = 40.30;
-            week.Week_type = "Weekday";
-            week.Days = "Monday,Tuesday,Wed";
-            //WeekdayDAO.insertWeek(week);
+            chSat.IsEnabled = false;
+            chSun.IsEnabled = false;
 
-            //WeekdayDAO.deleteWeek("week 4");
+            PopulateTable(WeekdayDAO.getAll());
 
-            WeekdayDAO.updateWeek("week 3",week);
+            
+
         }
 
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Weekday weekday = new Weekday();
+            weekday.Title = tbTitle.Text;
+            weekday.Week_type = cbWeek.Text;
+
+            double time = (double.Parse(tbHours.Text)) * 60 + double.Parse(tbMinutes.Text);
+            weekday.Hours = time;
+
+            ArrayList daysList = new ArrayList();
+            int daycount=0;
+            if ((bool)chMon.IsChecked)
+            { 
+                daysList.Add("Monday");
+                daycount++;
+            }
+            if ((bool)chTue.IsChecked)
+            {   
+                daysList.Add("Tuesday");
+                daycount++;
+            }
+            if ((bool)chWed.IsChecked) 
+            {  
+                daysList.Add("Wednesday");
+                daycount++;
+            }
+            if ((bool) chThu.IsChecked)
+            {   
+                daysList.Add("Thursday");
+                daycount++;
+            }
+            if ((bool)chFri.IsChecked)
+            {  
+                daysList.Add("Friday");
+                daycount++;
+            }
+            if ((bool)chSat.IsChecked)
+            {
+                daysList.Add("Saturday");
+                daycount++;
+            }
+            if ((bool)chSun.IsChecked)
+            {  
+                daysList.Add("Sunday");
+                daycount++;
+            }
+
+
+            string[] myArray = (string[])daysList.ToArray(typeof(string));
+            string str = string.Join(",",myArray);
+
+            Console.WriteLine("=="+str);
+
+            weekday.No_days = daycount;
+            weekday.Days = str;
+
+            /*string[] daysArray = str.Split(',');
+
+            foreach(string x in daysArray)
+            {
+                Console.WriteLine(x);
+            }*/
+
+            int tuid = cbSlots.SelectedIndex;
+            double timeslot = 0;
+            if (tuid == 0)
+                timeslot = 30;
+            else if (tuid == 1)
+                timeslot = 60;
+            else if (tuid == 2)
+                timeslot = 120;
+            else if (tuid == 3)
+                timeslot = 180;
+
+            weekday.Slots = timeslot;
+
+            WeekdayDAO.insertWeek(weekday);
+            PopulateTable(WeekdayDAO.getAll());
+
+        }
+
+        private void PopulateTable(List<Weekday> list)
+        {
+            //List<Weekday> list = WeekdayDAO.getAll();
+
+            var observableList = new ObservableCollection<Weekday>();
+            list.ForEach(x => observableList.Add(x));
+
+            listView.ItemsSource = observableList;
+        }
+
+        private void cbWeek_DropDownClosed(object sender, EventArgs e)
+        {
+
+            if (cbWeek.SelectedIndex == 0)
+            {
+                chSat.IsEnabled = false;
+                chSun.IsEnabled = false;
+                chMon.IsEnabled = true;
+                chTue.IsEnabled = true;
+                chWed.IsEnabled = true;
+                chThu.IsEnabled = true;
+                chFri.IsEnabled = true;
+            }
+            else if (cbWeek.SelectedIndex == 1)
+            {
+                chSat.IsEnabled = true;
+                chSun.IsEnabled = true;
+                chMon.IsEnabled = false;
+                chTue.IsEnabled = false;
+                chWed.IsEnabled = false;
+                chThu.IsEnabled = false;
+                chFri.IsEnabled = false;
+            }
+        }
+
+        private void searchField_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (searchField.Text.Equals(""))
+            {
+                PopulateTable(WeekdayDAO.getAll());
+            }
+            else
+            {
+                PopulateTable(WeekdayDAO.search(searchField.Text));
+            }
+        }
+
+     
+        private void listview_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Weekday week = (Weekday)listView.SelectedItem;
+            Console.WriteLine(week.Title);
+        }
     }
 }
