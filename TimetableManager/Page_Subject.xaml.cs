5db59f3e5e5d0a6a07a6b304812aa1c2e5b240df
MOVEENA RAJAPAKSE <moveenapadmapali@gmail.com>
2020-08-17 09:55:27 +0530

    sub add and view

--- TimetableManager/Page_Subject.xaml.cs
+++ TimetableManager/Page_Subject.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TimetableManager.SubjectDAO;
 
 namespace TimetableManager
 {
@@ -23,10 +25,44 @@ namespace TimetableManager
         public Page_Subject()
         {
             InitializeComponent();
+            PopulateTable(SubjectDetailsDAO.getAll());
         }
 
         private void searchField_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (searchField.Text.Equals(""))
+            {
+                PopulateTable(SubjectDetailsDAO.getAll());
+            }
+            else
+            {
+                PopulateTable(SubjectDetailsDAO.search(searchField.Text));
+            }
+        }
+
+        private void PopulateTable(List<Subject> list)
+        {
+            var observableList = new ObservableCollection<Subject>();
+            list.ForEach(x => observableList.Add(x));
+
+            listView.ItemsSource = observableList;
+        }
+
+        private void btnSubAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Subject subject = new Subject();
+
+            subject.Year = cmbYear.Text;
+            subject.Semester = cmbSem.Text;
+            subject.SubName = txtSub.Text;
+            subject.SubCode = txtCode.Text;
+            subject.LecHrs = double.Parse(txtLecHrs.Text);
+            subject.TuteHrs = double.Parse(txtTuteHrs.Text);
+            subject.LabHrs = double.Parse(txtLabHrs.Text);
+            subject.EvaluHrs = double.Parse(txtEvaHrs.Text);
+
+            SubjectDetailsDAO.insertSubjects(subject);
+            PopulateTable(SubjectDetailsDAO.getAll());
 
         }
     }
