7141f0c8024144133fb6c0ed021656e4b03f4b91
Yasangi <sanduni98y@gmail.com>
2020-08-19 15:07:41 +0530

    Student and Group Completed

--- TimetableManager/GroupDAO/GroupDetailsDAO.cs
+++ TimetableManager/GroupDAO/GroupDetailsDAO.cs
@@ -18,7 +18,7 @@ namespace TimetableManager.GroupDAO
 
         //=======================================Insert Group===================
 
-        public static void insertgroups(Group group)
+        public static void insertgroups(Groups group)
         {
             using (SQLiteConnection conn = new SQLiteConnection(App.connString))
             {
@@ -26,7 +26,7 @@ namespace TimetableManager.GroupDAO
                 {
                     conn.Open();
                     SQLiteCommand command = new SQLiteCommand(conn);
-                    command.CommandText = "INSERT INTO Group (academic_id,student_count,group_no,group_id,subgroup_no,subgroup_id) VALUES (@acdemicid,@studentcount,groupno,groupid,subgroupno,subgroupid)";
+                    command.CommandText = "INSERT INTO Groups_Info (academic_id,student_count,group_no,group_id,subgroup_no,subgroup_id) VALUES (@academicid,@studentcount,@groupno,@groupid,@subgroupno,@subgroupid)";
 
                     command.Parameters.AddWithValue("@academicid", group.AcademicId);
                     command.Parameters.AddWithValue("@studentcount", group.StudentCount);
@@ -38,7 +38,7 @@ namespace TimetableManager.GroupDAO
                     var t = command.ExecuteNonQuery();
                     MessageBox.Show("Successfully Added");
                 }
-                catch(SQLiteException e)
+                catch (SQLiteException e)
                 {
                     MessageBox.Show("Error occured in inserting " + e.Message);
                 }
@@ -48,20 +48,20 @@ namespace TimetableManager.GroupDAO
 
 
         //===============================Load Groups======================
-        public static List<Group> getAll()
+        public static List<Groups> getAll()
         {
-            List<Group> GrpList = new List<Group>();
+            List<Groups> GrpList = new List<Groups>();
             using (SQLiteConnection conn = new SQLiteConnection(App.connString))
             {
                 try
                 {
                     conn.Open();
                     SQLiteCommand command = new SQLiteCommand(conn);
-                    command.CommandText = @"SELECT academic_id,student_count,group_no,group_id,subgroup_no,subgroup_id  FROM  Group";
+                    command.CommandText = @"SELECT academic_id,student_count,group_no,group_id,subgroup_no,subgroup_id  FROM  Groups_Info";
                     SQLiteDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
-                        Group group = new Group();
+                        Groups group = new Groups();
                         group.AcademicId = reader["academic_id"].ToString();
                         group.StudentCount = int.Parse(reader["student_count"].ToString());
                         group.Groupno = int.Parse(reader["group_no"].ToString());
@@ -74,7 +74,7 @@ namespace TimetableManager.GroupDAO
 
                     }
                 }
-                catch(SQLiteException e)
+                catch (SQLiteException e)
                 {
                     MessageBox.Show("Error in Loading " + e.Message);
                 }
@@ -83,5 +83,93 @@ namespace TimetableManager.GroupDAO
             return GrpList;
 
         }
+
+        //===============================Update Groups=====================
+        public static void updategroups(string ngroupid, Groups groups)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.connString))
+            {
+                try
+                {
+                    conn.Open();
+                    SQLiteCommand command = new SQLiteCommand(conn);
+                    command.CommandText = "UPDATE Groups_Info " +
+                        "SET academic_id = @academicid," +
+                               "student_count = @studentcount," +
+                               "group_no = @groupno," +
+                               "group_id = @groupid," +
+                               "subgroup_no = @subgroupno," +
+                               "subgroup_id = @subgroupid " +
+                               "WHERE group_id = @ngroupid";
+                    command.Parameters.AddWithValue("@ngroupid", ngroupid);
+                    command.Parameters.AddWithValue("@academicid", groups.AcademicId);
+                    command.Parameters.AddWithValue("@studentcount", groups.StudentCount);
+                    command.Parameters.AddWithValue("@groupno", groups.Groupno);
+                    command.Parameters.AddWithValue("@groupid", groups.GroupId);
+                    command.Parameters.AddWithValue("@subgroupno", groups.SubGroupno);
+                    command.Parameters.AddWithValue("@subgroupid", groups.SubGroupId);
+
+                    var t = command.ExecuteNonQuery();
+
+
+
+                }
+                catch (SQLiteException e)
+                {
+                    MessageBox.Show("Error in updating" + e.Message);
+                }
+            }
+        }
+
+        public static void deletegroups(string ngroupid)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.connString))
+            {
+                try
+                {
+                    conn.Open();
+                    SQLiteCommand command = new SQLiteCommand(conn);
+
+                    command.CommandText = " DELETE FROM Groups_Info  WHERE group_id = @groupid";
+                    command.Parameters.AddWithValue( "@groupid", ngroupid);
+
+                    var s = command.ExecuteNonQuery();
+
+                }
+                catch (SQLiteException e)
+                {
+                    MessageBox.Show("Error in Deleting" + e.Message);
+                }
+            }
+        }
+
+        public static List<Groups> searchgroups(string groupid)
+        {
+            List<Groups> GrpList = new List<Groups>();
+            using (SQLiteConnection conn = new SQLiteConnection(App.connString))
+            {
+                conn.Open();
+                SQLiteCommand command = new SQLiteCommand(conn);
+                command.CommandText = "SELECT academic_id,group_id,subgroup_id FROM Groups_Info WHERE group_id LIKE '%" + groupid + "%'";
+
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Groups groups = new Groups();
+                    groups.AcademicId = reader["academic_id"].ToString();
+                    groups.GroupId = reader["group_id"].ToString();
+                    groups.SubGroupId = reader["subgroup_id"].ToString();
+
+
+                    GrpList.Add(groups);
+
+                }
+            }
+
+            return GrpList;
+        }
+    
     }
+
+    
 }
--- TimetableManager/Page_Tags.xaml.cs
+++ TimetableManager/Page_Tags.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TimetableManager.TagDAO;
 
 namespace TimetableManager
 {
@@ -24,5 +26,76 @@ namespace TimetableManager
         {
             InitializeComponent();
         }
+
+        private void addtag_Click(object sender, RoutedEventArgs e)
+        {
+            Tag tag = new Tag();
+            tag.Tags = txttag.Text;
+
+            TagDetailsDAO.inserttag(tag);
+            PopulateTableTag(TagDetailsDAO.getAll());
+            cleartags();
+        }
+
+        private void PopulateTableTag(List<Tag> list)
+        {
+
+            var observableList = new ObservableCollection<Tag>();
+            list.ForEach(x => observableList.Add(x));
+
+            listViewtag.ItemsSource = observableList;
+        }
+
+        private void cleartags()
+        {
+            txttag.Text = "";
+        }
+
+       
+
+        private void deletetag_Click(object sender, RoutedEventArgs e)
+        {
+            Tag tg = (Tag)listViewtag.SelectedItem;
+            TagDetailsDAO.deleteTags(tg.Tags);
+            PopulateTableTag(TagDetailsDAO.getAll());
+            cleartags();
+
+        }
+
+        private void updatetag_Click(object sender, RoutedEventArgs e)
+        {
+            Tag tgt = (Tag)listViewtag.SelectedItem;
+
+            Tag tag = new Tag();
+            tag.Tags = txttag.Text;
+
+            TagDetailsDAO.updateTag(tgt.Tags, tag);
+
+            PopulateTableTag(TagDetailsDAO.getAll());
+        }
+
+        private void listViewtag_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            cleartags();
+
+            Tag tag = (Tag)listViewtag.SelectedItem;
+
+            if (tag != null)
+                txttag.Text = tag.Tags;
+        }
+
+        private void searchFieldtag_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txttag.Text.Equals(""))
+            {
+                PopulateTableTag(TagDetailsDAO.getAll());
+            }
+            else
+            {
+                PopulateTableTag(TagDetailsDAO.searchTags(txttag.Text));
+            }
+        }
+
+       
     }
 }
