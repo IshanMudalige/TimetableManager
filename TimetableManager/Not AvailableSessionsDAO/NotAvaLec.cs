4fcd5faefcabf0eb9f22b890e5ea3c94ffd3310b
Yasangi <sanduni98y@gmail.com>
2020-09-20 21:05:41 +0530

    Not Availble LEC DAO

---
+++ TimetableManager/Not AvailableSessionsDAO/NotAvaLec.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimetableManager.Not_AvailableSessionsDAO
+{
+    class NotAvaLec
+    {
+        private int lec_id;
+        private string lec_name;
+        private double time;
+        private string faculty;
+        private string department;
+        private string center;
+
+        public NotAvaLec()
+        {
+        }
+
+        public NotAvaLec(int lecId, string lecname, double time, string faculty ,string department , string center)
+        {
+
+            this.lec_id = lecId;
+            this.lec_name = lecname;
+            this.time = time;
+            this.faculty = faculty;
+            this.department = department;
+            this.center = center;
+        }
+
+        public int LecID { get => lec_id; set => lec_id = value; }
+
+        public string LecName { get => lec_name; set => lec_name = value; }
+
+        public double Lectime { get => time; set => time = value; }
+
+        public string Faculty { get => faculty; set => faculty = value; }
+
+        public string Department { get => department; set => department = value; }
+
+        public string Center{ get => center; set => center = value; }
+
+    }
+
+    
+}
--- TimetableManager/Page_Session.xaml.cs
+++ TimetableManager/Page_Session.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TimetableManager.Not_AvailableSessionsDAO;
 
 namespace TimetableManager
 {
@@ -23,8 +26,178 @@ namespace TimetableManager
         public Page_Session()
         {
             InitializeComponent();
+            loadfacultyCombo();
+            loaddepartmentCombo();
+            loadcenterCombo();
+            loadtimeCombobox();
+            PopulatenotavailableLec(NotAvaLecDao.getAll());
+          
         }
 
-        
+        private void PopulatenotavailableLec(List<NotAvaLec> list)
+        {
+
+
+            var observableList = new ObservableCollection<NotAvaLec>();
+            list.ForEach(x => observableList.Add(x));
+
+            listView_Copy.ItemsSource = observableList;
+
+        }
+
+        private void PopulateTableLectuers(List<NotAvaLec> list)
+        {
+            var observableList = new ObservableCollection<NotAvaLec>();
+            list.ForEach(x => observableList.Add(x));
+
+            listView.ItemsSource = observableList;
+        }
+
+        //===============================Faculty Combo Box=====================================
+
+        public void loadfacultyCombo()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.connString))
+            {
+                try
+                {
+                    conn.Open();
+                    SQLiteCommand command = new SQLiteCommand(conn);
+                    command.CommandText = @"SELECT faculty FROM Lecturer";
+                    SQLiteDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string faculty = reader["faculty"].ToString();
+                        selectfaculty.Items.Add(faculty);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("-" + e);
+                }
+            }
+        }
+
+        //=============================================Department Combo Box==========================================
+
+        public void loaddepartmentCombo()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.connString))
+            {
+                try
+                {
+                    conn.Open();
+                    SQLiteCommand command = new SQLiteCommand(conn);
+                    command.CommandText = @"SELECT department FROM Lecturer";
+                    SQLiteDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string dep = reader["department"].ToString();
+                        selectDepartment.Items.Add(dep);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("-" + e);
+                }
+            }
+        }
+
+        //==========================================Center Combo Box=====================================================
+
+        public void loadcenterCombo()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.connString))
+            {
+                try
+                {
+                    conn.Open();
+                    SQLiteCommand command = new SQLiteCommand(conn);
+                    command.CommandText = @"SELECT center FROM Lecturer";
+                    SQLiteDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string ct = reader["center"].ToString();
+                        selectCenter.Items.Add(ct);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("-" + e);
+                }
+            }
+        }
+
+        //==========================================Time Combo Box=====================================================
+
+        public void loadtimeCombobox()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(App.connString))
+            {
+                try
+                {
+                    conn.Open();
+                    SQLiteCommand command = new SQLiteCommand(conn);
+                    command.CommandText = @"SELECT hours FROM Weekday_Days";
+                    SQLiteDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string t = reader["hours"].ToString();
+                        selectnotavailabletime.Items.Add(t);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("-" + e);
+                }
+            }
+        }
+        //=========================================================Retrive Specific LECTUERS========================================== 
+        private void submit_Click(object sender, RoutedEventArgs e)
+        {
+            NotAvaLec notAvaLec =  new NotAvaLec() ;
+
+            notAvaLec.Faculty = selectfaculty.Text;
+            notAvaLec.Department = selectDepartment.Text;
+            notAvaLec.Center = selectCenter.Text;
+            PopulateTableLectuers(NotAvaLecDao.getAllLec(notAvaLec.Faculty,notAvaLec.Department,notAvaLec.Center ));
+            
+        }
+
+        private void NAL_ADD_BTN_Click(object sender, RoutedEventArgs e)
+        {
+            NotAvaLec notAvaLec =  (NotAvaLec)listView.SelectedItem;
+            if(notAvaLec != null)
+            {
+                NotAvaLec avaLec = new NotAvaLec();
+                avaLec.Lectime = double.Parse(selectnotavailabletime.Text);
+                NotAvaLecDao.insertnotAvailableLec(notAvaLec.LecID,notAvaLec.LecName,avaLec);
+                PopulatenotavailableLec(NotAvaLecDao.getAll());
+            }
+           
+            
+           
+        }
+
+       
+
+      
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            NotAvaLec avaLec = (NotAvaLec)listView.SelectedItem;
+
+            if (avaLec != null)
+            {
+                selectnotavailabletime.Text = avaLec.Lectime.ToString();
+               
+               
+            }
+           
+
+            
+            
+        }
     }
 }
